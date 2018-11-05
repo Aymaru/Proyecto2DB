e269d9ba0c3da84e76357c09f9d644b0fb64d3b4
Aymaru <aycastillo@ic-itcr.ac.cr>
2018-11-05 03:51:29 -0600

    Script db completo, programa para migrar completo

---
+++ README.txt
@@ -0,0 +1,28 @@
+Migracion de base de datos de Excel a MySQL
+
+Base de datos -> MySQL Server 8.0
+Lenguaje -> Python 3.7
+
+Instalar las siguientes librerias para python
+
+xlrd
+pymysql
+
+Se pueden instalar desde el shell de windows
+
+pip install xlrd
+pip install pymysql
+
+Es necesario tener agregado python en las variables de ambiente ( Esto se realiza en la instalacion o pueden hacerlo manualmente )
+
+host = "localhost"
+usuario = "root"
+pwd = "root"
+puerto = 3307
+
+Para crear la base de datos:
+
+1. Abrir un query en MySQL
+2. Abrir el script Proyecto2DB.sql
+3. Ejecutar todo el query
+4. Ejecutar el programa migrarBD_excel_a_mysql.py
--- migrarBD_excel_a_mysql.py
+++ migrarBD_excel_a_mysql.py
@@ -2,62 +2,146 @@ import xlrd
 import pymysql
 import os
 
-# Open the workbook and define the worksheet
-pathxls = "C:\\Users\\ayma-93\\Documents\\Semestre II - 2018\\Diseno de Software\\Tareas\\Proyecto2DB\\acc.xls"
+# Abrir archivo de excel y hoja de trabajo
 
-#book = xlrd.open_workbook(os.getcwd()+"\\acc.xlsx")
-#sheet = book.sheet_by_name("acc")
+print("Leyendo del archivo xls ...\n")
 
-# Establish a MySQL connection
-database = pymysql.connect (host='localhost', port =3307, user = 'root', password = 'root')#, database = 'proyecto2db' )
+book = xlrd.open_workbook(os.getcwd()+"\\acc.xlsx")
+sheet = book.sheet_by_name("acc")
 
-# Get the cursor, which is used to traverse the database, line by line
+
+provincia = {}
+canton  = {}
+distrito  = {}
+rol = {}
+lesion = {}
+edad_quinquenal = {}
+
+for r in range (1,sheet.nrows):
+    
+    if sheet.cell(r,0) == sheet.show_zero_values:
+        break    
+    if not(sheet.cell(r,1).value in provincia):
+        provincia[sheet.cell(r,1).value] = sheet.cell(r,2).value
+    if not(sheet.cell(r,3).value in canton):
+        canton[sheet.cell(r,3).value]  = len(canton) + 1
+    if not(sheet.cell(r,4).value in distrito):
+        distrito[sheet.cell(r,4).value] = len(distrito) + 1
+    if not(sheet.cell(r,8).value in rol):
+        rol[sheet.cell(r,8).value] = sheet.cell(r,9).value
+    if not(sheet.cell(r,10).value in lesion):
+        lesion[sheet.cell(r,10).value] = sheet.cell(r,11).value
+    if not(sheet.cell(r,13).value in edad_quinquenal):
+        edad_quinquenal[sheet.cell(r,13).value] = len(edad_quinquenal) + 1
+
+provincia.pop('')
+rol.pop('')
+lesion.pop('')
+canton.pop('')
+distrito.pop('')
+edad_quinquenal.pop('')
+
+print(sheet.nrows)
+print (sheet.ncols)
+print(provincia)
+print(canton)
+print(distrito)
+print(rol)
+print(lesion)
+print(edad_quinquenal)
+
+print("Leído archivo xls ...\n")
+
+
+# Establecer conexión con MySQL
+print ("Conectandose a la base de datos ...\n")
+database = pymysql.connect (host='localhost', port =3307, user = 'root', password = 'root', database = 'proyecto2db')
+
+print ("Conectado a la base de datos ...\n")
+
+# Obtener el cursor que se utilizara para ingresar datos, linea por linea
 cursor = database.cursor()
 
-# Create the INSERT INTO sql query
-query = """INSERT INTO orders (product, customer_type, rep, date, actual, expected, open_opportunities, closed_opportunities, city, state, zip, population, region) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+# Creación de querys e incersionces en la base de datos
+print( "Insertando en tabla de provincia ... \n")
+query = "INSERT INTO Provincia (idProvincia, nombre) VALUES (%s, %s)"
 
-print ("Conectado a la base de datos")
+elementos = provincia.items()
+for ID, nombre in elementos:
+    values = (int(ID), nombre)
+    cursor.execute(query, values)
 
-cursor.execute("SHOW DATABASES")
-l = cursor.fetchall()
-print (l)
-l = [ i[0] for i in l ]
+print( "Insertando en tabla de canton ... \n")
+query = "INSERT INTO Canton (idCanton, nombre) VALUES (%s, %s)"
 
+for nombre, ID in canton.items():
+    values = (int(ID), nombre)
+    cursor.execute(query, values)
 
+print( "Insertando en tabla de distrito ... \n")
+query = "INSERT INTO Distrito (idDistrito, nombre) VALUES (%s, %s)"
 
-# Create a For loop to iterate through each row in the XLS file, starting at row 2 to skip the headers
-##for r in range(1, sheet.nrows):
-##		product		= sheet.cell(r,).value
-##		customer	= sheet.cell(r,1).value
-##		rep			= sheet.cell(r,2).value
-##		date		= sheet.cell(r,3).value
-##		actual		= sheet.cell(r,4).value
-##		expected	= sheet.cell(r,5).value
-##		open		= sheet.cell(r,6).value
-##		closed		= sheet.cell(r,7).value
-##		city		= sheet.cell(r,8).value
-##		state		= sheet.cell(r,9).value
-##		zip			= sheet.cell(r,10).value
-##		pop			= sheet.cell(r,11).value
-##		region	= sheet.cell(r,12).value
-##
-##		# Assign values from each row
-##		values = (product, customer, rep, date, actual, expected, open, closed, city, state, zip, pop, region)
-##
-##		# Execute sql Query
-##		cursor.execute(query, values)
-##
-### Close the cursor
+for nombre,ID in distrito.items():
+    values = (int(ID), nombre)
+    cursor.execute(query, values)
+
+print( "Insertando en tabla de rol ... \n")
+query = "INSERT INTO RolAfectado (idRolAfectado, descripcion) VALUES (%s, %s)"
+
+elementos = rol.items()
+for ID, descripcion in elementos:
+    values = (int(ID), descripcion)
+    cursor.execute(query, values)
+
+print( "Insertando en tabla de lesion ... \n")
+query = "INSERT INTO TipoLesion (idTipoLesion, descripcion) VALUES (%s, %s)"
+
+elementos = lesion.items()
+for ID, descripcion in elementos:
+    values = (int(ID), descripcion)
+    cursor.execute(query, values)
+
+print( "Insertando en tabla de edad quinquenal ... \n")
+query = "INSERT INTO EdadQuinquenal (idEdadQuinquenal, descripcion) VALUES (%s, %s)"
+
+for nombre,ID in edad_quinquenal.items():
+    values = (int(ID), nombre)
+    cursor.execute(query, values)
+
+print( "Insertando en tabla de accidente ... \n")
+query = "INSERT INTO Accidente (idAccidente, idProvincia, idCanton, idDistrito, Dia, Mes, Anno, idRolAfectado, idTipoLesion, edadAfectado, idEdadQuinquenal, Sexo) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" #{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}) " #(
+ID = 1
+for registro in range(1, sheet.nrows):
+    
+    idAccidente = int(ID)
+    if sheet.cell(registro,1).value == '':
+        break
+    idProvincia = int(sheet.cell(registro,1).value)
+    
+    idCanton = int(canton.get(sheet.cell(registro,3).value))
+    idDistrito = int(distrito.get(sheet.cell(registro,4).value))
+    Dia = str(sheet.cell(registro,5).value)
+    Mes = str(sheet.cell(registro,6).value)
+    Anno = str(sheet.cell(registro,7).value)
+    idRolAfectado = int(sheet.cell(registro,8).value)
+    idTipoLesion = int(sheet.cell(registro,10).value)
+    edadAfectado = str(sheet.cell(registro,12).value)
+    idEdadQuinquenal = int(edad_quinquenal.get(sheet.cell(registro,13).value))
+    Sexo = str(sheet.cell(registro,15).value)
+    values = (idAccidente, idProvincia, idCanton, idDistrito, Dia, Mes, Anno, idRolAfectado, idTipoLesion, edadAfectado, idEdadQuinquenal, Sexo)
+    cursor.execute(query, values)
+    ID = ID + 1
+
+### Cerrar el cursor
 cursor.close()
 
-# Commit the transaction
-#database.commit()
+# Commit de la transación
+database.commit()
 
-# Close the database connection
+print ("Cerrar conexion a la base de datos ...\n")
+# Cerrar la conexión con la base de datos
 database.close()
 
-### Print results
-##print ""
-##print ""
-##print ""
+print ("Conexin cerrada ...\n")
+
+print( "Migracion completada... \n")
